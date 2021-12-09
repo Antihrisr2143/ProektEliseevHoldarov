5db1aca1c133683ac9aa94bef022013b070d0163
Elis_dim <eliseevdima.pol@gmail.com>
2021-12-09 22:56:55 +0700

    test2.2

---
+++ test2/test2.2.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <iomanip>
+
+#include <math.h>
+using namespace std;
+
+struct rabotnik {
+	char* name;
+	int id, age;
+	char sex;
+	char post;
+	int opening_hours;
+	double prize;
+
+};
+rabotnik s1;
+s1.name = "Grek";
+s1.sex = 'm';
+s1.id = 1;
+s1.age = 18;
+s1.post = rab;
+s1.opening_hors = 70;
+s1.prize = 1500;
+
+
+
+
+
+
