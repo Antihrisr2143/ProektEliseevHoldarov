c4b379fc0a1cff760293c441cb854f877a3d1973
Elis_dim <eliseevdima.pol@gmail.com>
2021-12-09 22:49:17 +0700

    rek

---
+++ test2/test2.1.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <iomanip>
+
+#include <math.h>
+using namespace std;
+
+struct rabotnik {
+	char* name;
+	int id, age;
+	char sex;
+	char post;
+	int opening_hours;
+	double prize;
+	char post;
+
+};
+
+
+
+
+
