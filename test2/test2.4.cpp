86f3932e6355d0ada39fc82b80baef6bde5ec70b
Elis_dim <eliseevdima.pol@gmail.com>
2021-12-17 00:16:35 +0700

    test2.4

---
+++ test2/test2.4.cpp
@@ -0,0 +1,198 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+#include <iostream>
+
+    using namespace std;
+
+    struct Rubotnick
+    {
+        int id;
+        int age;
+        char name; 
+        char post;
+        char How_many_days_does_it_work;
+
+
+    };
+
+    int main()
+    {
+        int  a;
+        Rubotnick rub1;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post = 
+            rub.How_many_days_does_it_work = 
+
+
+        std::cout << rub1.id << "\t" << rub1.age << std::endl;
+
+        Rubotnick rub2;
+
+        rub2.id = 20;
+        rub2.age = 30;
+
+        Rubotnick rub3;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub4;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub5;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub6;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub7;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub8;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub9;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub10;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub11;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub12;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub13;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub14;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub15;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub16;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub17;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub18;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub19;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+            Rubotnick rub20;
+
+        rub.id = ;
+        rub.age = ;
+        rub.name =
+            rub.post =
+            rub.How_many_days_does_it_work =
+
+          
+
+        int* mass = new int[20];
+
+        Rubotnick* m = new Rubotnick[20];
+
+        m[0].id;
+        m[1].id;
+
+       
+
+        return 0;
+    }
+}
---
+++ test2/test2.5.cpp
@@ -0,0 +1,260 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+#include <iostream>
+
+    using namespace std;
+
+    struct Rubotnick
+    {
+        int id;
+        int age;
+        char name; 
+        char post;
+        int How_many_days_does_it_work;
+
+
+    };
+
+    int main()
+    {
+        int  a;
+        Rubotnick rub1;
+
+        rub.id =1;
+        rub.age =27;
+        rub.name = Kiril_petruhin;
+        rub.post = auxiliary worker;
+            rub.How_many_days_does_it_work = 270;
+
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+        Rubotnick rub2;
+
+        rub2.id = 2;
+        rub2.age = 30;
+        rub.name =Zarkis_merkovich ;
+        rub.post = auxiliary worker;
+            rub.How_many_days_does_it_work = 365;
+
+            std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+        Rubotnick rub3;
+
+        rub.id =3 ;
+        rub.age = 33;
+        rub.name = Said_holdarov
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 573;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub4;
+
+        rub.id = 4;
+        rub.age = 29;
+        rub.name = Anton_vasikin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 244;
+        
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub5;
+
+        rub.id =5 ;
+        rub.age =40 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 1235;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub6;
+
+        rub.id =6 ;
+        rub.age =49 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 3675;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub7;
+
+        rub.id =7 ;
+        rub.age =48 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 5327;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub8;
+
+        rub.id =8 ;
+        rub.age =19 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 100;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub9;
+
+        rub.id =9 ;
+        rub.age =53 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 8325;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub10;
+
+        rub.id =10 ;
+        rub.age =35 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 744;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub11;
+
+        rub.id =11 ;
+        rub.age =37 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 274;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub12;
+
+        rub.id =12 ;
+        rub.age =59 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 322;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub13;
+
+        rub.id =13 ;
+        rub.age =44 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 8871;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub14;
+
+        rub.id =14 ;
+        rub.age =56 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 10537;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub15;
+
+        rub.id =15 ;
+        rub.age =34 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 257;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub16;
+
+        rub.id 16= ;
+        rub.age =37 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 649;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub17;
+
+        rub.id =17 ;
+        rub.age =48 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 7324;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub18;
+
+        rub.id =18 ;
+        rub.age =37 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 8888;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub19;
+
+        rub.id =19 ;
+        rub.age =22 ;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 277;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+            Rubotnick rub20;
+
+        rub.id =20 ;
+        rub.age = 25;
+        rub.name = Kiril_petruhin;
+            rub.post = auxiliary worker;
+        rub.How_many_days_does_it_work = 537;
+
+        std::cout << rub1.id << "\t" << rub1.age << rub.name << rub.post << rub.How_many_days_does_it_work << std::endl;
+
+          
+
+        int* mass = new int[20];
+
+        Rubotnick* m = new Rubotnick[20];
+
+        m[1].id;
+        m[2].id;
+        m[3].id;
+        m[4].id;
+        m[5].id;
+        m[6].id;
+        m[7].id;
+        m[8].id;
+        m[9].id;
+        m[10].id;
+        m[11].id;
+        m[12].id;
+        m[13].id;
+        m[14].id;
+        m[15].id;
+        m[16].id;
+        m[17].id;
+        m[18].id;
+        m[19].id;
+        m[20].id;
+        
+
+
+       
+
+        return 0;
+    }
+}
